ab3ed176ce08633dd48c9a712179ebb574ce110a
Abner Vargas Garcia <abnervgarcia@hotmail.com>
2025-08-04 16:18:58 -0600

    feature(report): Add orchestration function for cumulative report

--- reporting/cumulative_report.py
+++ reporting/cumulative_report.py
@@ -15,12 +15,48 @@ def request_data(bucket_name):
         Returns:  
     """
     logger = logging.getLogger(__name__)
-    logging.info(f"Downloading historical data from S3 bucket {bucket_name}")
+    # Define S3 path
     s3_path = f"s3://{bucket_name}/curated_data/**/*.parquet"
 
-    #Merge data
-
+    logger.info(f"Loading historical data from S3 bucket {bucket_name}")
+    # Merge data
     try:
         historical_df = pd.read_parquet(s3_path)
-        logger.info(f"Succesfully extracted data from S3 {bucket_name}")
+        logger.info(f"Successfully extracted {len(historical_df)} records")
+        return historical_df
+    except FileNotFoundError:
+        logger.warning(f"Failed to find data in S3 {bucket_name}, returning empty DataFrame")
+        return pd.DataFrame()  # Return an empty DataFrame if no data is found
+    except Exception as e:
+        logger.error(f"An error occurred while loading data from S3: {e}")
+        raise
+
+def generate_cumulative_report(config):
+    """
+    Generates a cumulative report by aggregating data from the S3 bucket.
+
+        Args:
+            bucket_name (str): The name of the S3 bucket containing the data.
+
+        Returns:
+            pd.DataFrame: A DataFrame containing the cumulative report.
+    """
+    logger = logging.getLogger(__name__)
+    
+    # Load historical data
+    s3_bucket = config['bucket_name']
+    all_data = request_data(s3_bucket)
+
+    logger.info(f"--- Starting Cumulative Report Generation ---")
+    if all_data.empty:
+        logger.info("No historical data found. Returning empty report.")
+        return pd.DataFrame()
+    
+    # This section performs data transformations
+
+    # This section produce different plots to use for the report
+
+    logger.info(f"--- Finished Cumulative Report Generation ---")
+
+
 
